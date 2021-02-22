3bcdd1bf6f2d4a8d71c82ac944e93f852672b949
HenryDiaz1202 <henrydiaz1202@gmail.com>
2021-02-22 19:52:21 +0000

    Ejercicio de la calculadora

---
+++ CALCULADORA/calculadora.php
@@ -0,0 +1,32 @@
+<?php
+$val_a = $_POST["valor_a"];
+$val_b = $_POST["valor_b"];
+
+$opcion = $_POST["rd"];
+
+switch($opcion){
+case 1:
+echo("LA SUMA DE LOS NUMEROS ES : "); 
+echo($val_a+$val_b);
+break;
+case 2:
+echo("LA RESTA DE LOS NUMEROS ES : "); 
+echo($val_a-$val_b);
+break;
+case 3: 
+if($val_b == 0)
+{
+echo("No se puede dividir entre 0");
+} 
+else
+{
+echo("LA DIVISION DE LOS NUMEROS ES : "); 
+echo($val_a/$val_b);
+}
+break;
+case 4: 
+echo("LA MULTIPLICACION DE LOS NUMEROS ES : "); 
+echo($val_a*$val_b);
+break;
+}
+?>
---
+++ CALCULADORA/main.js
@@ -0,0 +1,4 @@
+function radio_btn(){
+    document.querySelectorAll('name=rd').forEach((x) => x.checked=false return(x));
+}
+
